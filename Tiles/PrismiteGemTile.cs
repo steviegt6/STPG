9ab85520bcbe27be41a88969d91aa9b30877f51e
Stevie <xxlennygamerxx@gmail.com>
2020-03-10 14:09:22 -0700

    Prismite Tile Skeleton

---
+++ Tiles/PrismiteGemTile.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace STPG.Tiles
+{
+	public class PrismiteGemTile : ModTile
+	{
+		public override void SetDefaults()
+		{
+			Main.tileSolid[Type] = true;
+			Main.tileStone[Type] = true;
+			Main.tileBlockLight[Type] = true;
+			Main.tileSpelunker[Type] = true;
+			Main.tileShine2[Type] = true;
+			Main.tileShine[Type] = 1200;
+			//dustType = int;
+			soundType = 21;
+			soundStyle = 2;
+			drop = mod.ItemType("Prismite Gem");
+			Main.tileSpelunker[Type] = true;
+		}
+	}
+}
