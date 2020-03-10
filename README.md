e32cb201c54ba9cb4066d82ed5e08e18c509cafe
Stevie <xxlennygamerxx@gmail.com>
2020-03-10 13:54:42 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# STPG
+ Stevie's Thorium Prismite Gems - A tModLoader Mod for Terraria. Download off the Mod Browser.
---
+++ STPG.cs
@@ -0,0 +1,11 @@
+using Terraria.ModLoader;
+
+namespace STPG
+{
+	public class STPG : Mod
+	{
+		public STPG()
+		{
+		}
+	}
+}
